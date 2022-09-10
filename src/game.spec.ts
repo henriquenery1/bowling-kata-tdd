e415b966f60e51e2eb76a2df5efd9df6b122b92d
Henrique Nery <henriquenery07@gmail.com>
2022-09-10 00:10:26 -0300

    test: add rollMany method

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,9 +1,16 @@
-import { Game } from './game'
+import { Game } from "./game";
 
 let game = new Game();
 
 describe("Game", () => {
   it("should score 0 for gutter game ", () => {
+    rollMany(20, 0);
     expect(game.score()).toBe(0);
   });
 });
+
+function rollMany(plays: number, downPins: number): void {
+  for (let i = 0; i < plays; i++) {
+    game.roll(downPins);
+  }
+}
