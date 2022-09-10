03fff18c796c93f27ced465d91b2019a82598251
Henrique Nery <henriquenery07@gmail.com>
2022-09-10 00:18:33 -0300

    test: add validation all one game

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -3,10 +3,15 @@ import { Game } from "./game";
 let game = new Game();
 
 describe("Game", () => {
-  it("should score 0 for gutter game ", () => {
+  it("should score 0 for gutter game", () => {
     rollMany(20, 0);
     expect(game.score()).toBe(0);
   });
+
+  it('should score 20 for all ones game', () => {
+    rollMany(20, 1)
+    expect(game.score()).toBe(20)
+  })
 });
 
 function rollMany(plays: number, downPins: number): void {
