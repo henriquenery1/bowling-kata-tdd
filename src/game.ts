5f7f34d81fdb1f276ac194b52d2aa857ca2891e0
Henrique Nery <henriquenery07@gmail.com>
2022-09-12 10:38:21 -0300

    feat: add spare methods

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,39 @@
 export class Game {
   private totalPlays: number = 20;
   private rolls: number[] = new Array(this.totalPlays++).fill(0);
-  private currentRoll: number = 0
+  private currentRoll: number = 0;
+  frameIndex: number = 0;
+  private spareValue: number = 10;
 
   score(): number {
-    const score: number = this.rolls.reduce((a, b) => a + b, 0)
-    return score
+    let score: number = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.isSpare(this.frameIndex)) {
+        score = this.spareValue + this.spareBonus(this.frameIndex);
+        this.frameIndex += 2;
+      } else {
+        score += this.sumOfBallsInFrame(this.frameIndex);
+        this.frameIndex += 2;
+      }
+    }
+    return score;
+  }
+
+  isSpare(frameIndex: number): boolean {
+    return (
+      this.rolls[frameIndex] + this.rolls[frameIndex + 1] === this.spareValue
+    );
+  }
+
+  private spareBonus(frameIndex: number): number {
+    return this.rolls[frameIndex + 2];
+  }
+
+  private sumOfBallsInFrame(frameIndex: number): number {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
   }
 
   roll(downPins: number): void {
-    this.rolls[this.currentRoll++] = downPins
+    this.rolls[this.currentRoll++] = downPins;
   }
 }
