2eced476e3cc03bcc1e5900601f27ee2541b6b7c
Henrique Nery <henriquenery07@gmail.com>
2022-09-12 10:37:55 -0300

    test: add spare functions

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,6 +1,10 @@
 import { Game } from "./game";
 
-let game = new Game();
+let game: Game = new Game();
+
+beforeEach(() => {
+  game = new Game();
+});
 
 describe("Game", () => {
   it("should score 0 for gutter game", () => {
@@ -8,14 +12,25 @@ describe("Game", () => {
     expect(game.score()).toBe(0);
   });
 
-  it('should score 20 for all ones game', () => {
-    rollMany(20, 1)
-    expect(game.score()).toBe(20)
-  })
+  it("should score 20 for all ones game", () => {
+    rollMany(20, 1);
+    expect(game.score()).toBe(20);
+  });
+
+  it("should score 14 for spare followed by a 2 ball", () => {
+    rollSpare();
+    game.roll(2);
+    expect(game.score()).toBe(14);
+  });
 });
 
 function rollMany(plays: number, downPins: number): void {
   for (let i = 0; i < plays; i++) {
     game.roll(downPins);
   }
+}
+
+function rollSpare(): void {
+  game.roll(5);
+  game.roll(5);
 }
